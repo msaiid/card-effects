1766f7a0b111449e64ce8680fa6573e8a89f1f30
tezras <it.hill.tj@gmail.com>
2023-09-27 12:23:59 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "./App.css";
+import "swiper/css";
+import "swiper/css/effect-cards";
+
+import { EffectCards } from "swiper/modules";
+
+const photos = {
+  cat1: require("./img/cat1.jpg"),
+  cat2: require("./img/cat2.jpg"),
+  dog1: require("./img/dog1.jpg"),
+  cat3: require("./img/cat3.jpg"),
+  dog2: require("./img/dog2.jpg"),
+  cat4: require("./img/cat4.jpeg"),
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Swiper
+        effect="cards"
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="mySwiper"
+      >
+        <SwiperSlide className="slide">
+          <img src={photos.cat1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide className="slide">
+          <img src={photos.cat2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide className="slide">
+          <img src={photos.cat3} alt="" />
+        </SwiperSlide>
+        <SwiperSlide className="slide">
+          <img src={photos.cat4} alt="" />
+        </SwiperSlide>
+        <SwiperSlide className="slide">
+          <img src={photos.dog1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide className="slide">
+          <img src={photos.dog2} alt="" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
